371b5e2744491fba558260c2d2a661d369693b42
Baddam Snehith Reddy <badreddy@manh.com>
2025-09-11 10:03:31 +0530

    .

--- db_connection.js
+++ db_connection.js
@@ -1,7 +1,7 @@
 const mongooseDB = require('mongoose')
 
 async function db_connection(){
-    return await mongooseDB.connect('mongodb+srv://smcc89:9NZWozUtp5CVn3sQ@clusterx.5ycdph1.mongodb.net/Myntra')
+    // user user connection string
 }
 
 module.exports = db_connection
